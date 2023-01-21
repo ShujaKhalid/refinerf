a679eef604265af8843137b2602dc17b112306a3
Shuja Khalid <shujakhalid@gmail.com>
2023-01-21 13:41:39 -0500

    intrinsics working at 50k iterations - include in paper

--- ablation_runner.sh
+++ ablation_runner.sh
@@ -8,10 +8,10 @@ deform_dims=(3)
 time_dims=(0)
 deform_qty_arr=(3)
 deform_hidden_qty_arr=(64)
-deform_intrinsics_arr=(0)
-deform_extrinsics_arr=(1)
-# noise_pct_arr=(1.0 0.8 0.6 0.4 0.2 0.0)
-noise_pct_arr=(0.30 0.20 0.10 0.0)
+deform_intrinsics_arr=(1)
+noise_pct_arr=(0.5 0.25 0.0)
+deform_extrinsics_arr=(0)
+# noise_pct_arr=(0.30 0.20 0.10 0.0)
 iters=50000
 
 for scene in "${cases[@]}";
